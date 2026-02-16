b47d949d190a59457aa93757b3be3d78af81b038
KOLEKAR ABHISHEK SHANTARAM <122772887+06Kolekar@users.noreply.github.com>
2026-02-16 16:06:15 +0530

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -96,11 +96,11 @@ class Settings(BaseSettings):
     # Database
     # ------------------------------------------------------------------
     DATABASE_URL: str
-    DATABASE_HOST: str = "localhost"
+    DATABASE_HOST: str = "mysql.railway.internal"
     DATABASE_PORT: int = 3306
     DATABASE_USER: str = "root"
     DATABASE_PASSWORD: str
-    DATABASE_NAME: str = "tender_db"
+    DATABASE_NAME: str = "railway"
 
     # ------------------------------------------------------------------
     # Security
